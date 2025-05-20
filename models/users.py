9807c5b4bd0e644de3ef0688e3f70594cbad2b59
NSANTOM <nicolas_gabriel.santome_pazos@mercedes-benz.com>
2025-05-20 16:14:47 +0200

    user model created

---
+++ models/users.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List
+from pydantic import EmailStr
+from datetime import datetime
+
+class UserBase(SQLModel):
+    username: str = Field(unique=True)
+    email: EmailStr = Field(unique=True)
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, index=True)
+    email: EmailStr = Field(unique=True, index=True)
+    hashed_password : str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    todo_lists: List["TodoList"] = Relationship(back_populates="owner")
+
+
